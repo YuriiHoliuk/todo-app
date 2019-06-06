e1513c158801217cc54dcdc0bb7b6862eaf8a195
Yurii Holiuk <holiuk.yurii@pdffiller.team>
2019-06-06 23:01:04 +0300

    dynamic list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,85 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import { todos } from './data/todos';
-import { users } from './data/users';
 import { TodoList } from './components/TodoList';
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
-export function App() {
-  const usersMap = users.reduce((acc, user) => ({ ...acc, [user.id]: user }), {});
-  const todosWithUsers = todos.map(todo => ({ ...todo, user: usersMap[todo.userId] }));
+export class App extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <TodoList todos={todosWithUsers} />
-  );
+    this.state = {
+      todos: null,
+      users: null,
+      isTODOSLoaded: false,
+      isUsersLoaded: false,
+      loadingCount: 0,
+    };
+  }
+
+  loadData = () => {
+    this.makeRequest(`${BASE_URL}/todos`, this.handleTODOsLoad);
+    this.makeRequest(`${BASE_URL}/users`, this.handleUsersLoad);
+  };
+
+  handleTODOsLoad = xhr => () => {
+    this.setState(prevState => ({
+      todos: JSON.parse(xhr.responseText),
+      isTODOSLoaded: true,
+      loadingCount: --prevState.loadingCount
+    }));
+  };
+
+  handleUsersLoad = xhr => () => {
+    this.setState(prevState => ({
+      users: JSON.parse(xhr.responseText),
+      isUsersLoaded: true,
+      loadingCount: --prevState.loadingCount
+    }));
+  };
+
+  makeRequest(url, cb) {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', url);
+    xhr.addEventListener('load', cb(xhr));
+    xhr.send();
+    this.setState(prevState => ({ loadingCount: ++prevState.loadingCount }));
+  }
+
+  render() {
+    const {
+      isTODOSLoaded,
+      isUsersLoaded,
+      todos,
+      users,
+      loadingCount,
+    } = this.state;
+
+    const isLoaded = isTODOSLoaded && isUsersLoaded;
+    const isLoading = loadingCount !== 0;
+
+    const button = (
+      <button
+        type="button"
+        disabled={isLoaded}
+        onClick={this.loadData}
+      >
+        {isLoading ? 'Loading...' : 'Load data'}
+      </button>
+    );
+
+    if (!isLoaded) {
+      return button;
+    } else {
+      const usersMap = users.reduce((acc, user) => ({ ...acc, [user.id]: user }), {});
+      const todosWithUsers = todos.map(todo => ({ ...todo, user: usersMap[todo.userId] }));
+
+      return (
+        <>
+          {button}
+          <TodoList todos={todosWithUsers} />
+        </>
+      );
+    }
+  }
 }
